33d95369f9d36fa71aabeaf04aaf0f6d3f6d2359
CharlieH2 <charlierh8@gmail.com>
2026-05-19 16:43:53 +0100

    Inital Commit

---
+++ Main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/home')
+def home():
+    return render_template('home.html.jinja')
+
