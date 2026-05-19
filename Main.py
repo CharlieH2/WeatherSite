bd4ef3ea0bef642c498172f05fb86e6b3fb9328a
CharlieH2 <charlierh8@gmail.com>
2026-05-19 20:54:02 +0100

    Utilised base Jinja template and updated style.css

--- Main.py
+++ Main.py
@@ -1,17 +1,10 @@
+import os
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Index Page'
-
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
-
-@app.route('/home')
 def home():
     return render_template('home.html.jinja')
 
